f68d49af114a2072834d150c6c16055fe984a73f
Nexlucas <luka.santos1105@gmail.com>
2026-03-16 11:04:01 -0300

    mexendo com css

---
+++ Aulas/outras_aulas/cont.js
@@ -0,0 +1,11 @@
+let cont = 6
+for (let index = 0; index <= 5; index++) {
+    if (index == 5) {
+        console.log("Feliz ano novo")
+    }else {
+        cont = cont - 1
+        console.log(cont)
+    }
+    
+}
+
